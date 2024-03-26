e7504d0bde87929fdfe83294031508d5c9b5ba5a
JavierCostas81 <javiercostas1981@gmail.com>
2024-03-26 01:06:44 -0300

    Punto 7 casi finalizado

---
+++ src/tp1/ejercicio7/EjercicioSucesion.java
@@ -0,0 +1,27 @@
+package tp1.ejercicio7;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class EjercicioSucesion {
+    public static List<Integer> calcularSucesion(int n) {
+        List<Integer> integerList = new ArrayList<>(Arrays.asList(n));
+        calcularSucesionRecursivo(integerList, n);
+        return integerList;
+
+    }
+
+    private static void calcularSucesionRecursivo(List<Integer> lista, int n) {
+        if (n > 1) {
+            if (n % 2 == 0) {
+                lista.add(n/2);
+                calcularSucesionRecursivo(lista,n/2);
+            }
+            else {
+                lista.add((3*n)+1);
+                calcularSucesionRecursivo(lista,(3*n)+1);
+            }
+        }
+    }
+}
--- src/tp1/ejercicio7/TestArrayList.java
+++ src/tp1/ejercicio7/TestArrayList.java
@@ -3,19 +3,16 @@ package tp1.ejercicio7;
 
 import tp1.ejercicio3.Estudiante;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class TestArrayList {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args){
         int[] secuencia = {1,-55,5,-8,8,15,55,
                 65,89,94,49,59,52,2,45,53,21,8,99,55,65,89,94,49,
                 59,52,2,45,53,21,8,99,450,123,387,-589};
         //Ejercicio a)
         List<Integer> aList = new ArrayList<>();
-        long lInicio,lFin = 0;
+        long lInicio,lFin;
         lInicio = System.currentTimeMillis();
         for (Integer e: secuencia) {
             aList.add(e);
@@ -24,9 +21,9 @@ public class TestArrayList {
             System.out.println(e);
         }
         lFin =  System.currentTimeMillis();
-        Double lTotal = (double) (lFin - lInicio);
-        System.out.println(String.format("La lista tardó %s segundos en terminar", lTotal/1000));
-        long lLInicio,lLFin = 0;
+        double lTotal = (double) (lFin - lInicio);
+        System.out.println("La lista tardó " + lTotal/1000 + " segundos en terminar");
+        long lLInicio,lLFin;
         lLInicio = System.currentTimeMillis();
         //Ejericio b
         List<Integer> aLList = new LinkedList<>();
@@ -37,69 +34,116 @@ public class TestArrayList {
             System.out.println(e);
         }
         lLFin =  System.currentTimeMillis();
-        Double lLTotal = (double) (lLFin - lLInicio);
-        System.out.println(String.format("La lista linkeada tardó %s segundos en terminar", lLTotal/1000));
+        double lLTotal = (double) (lLFin - lLInicio);
+        System.out.println("La lista linkeada tardó " + lLTotal/1000 + " segundos en terminar");
 
         //Ejercicio c
 
-        long wInicio,wFin = 0;
+        long wInicio,wFin;
         wInicio = System.currentTimeMillis();
-        List tmp = new ArrayList<>(aList);
+        List<Integer> tmp = new ArrayList<>(aList);
         while (!tmp.isEmpty()) {
             System.out.println(tmp.get(0));
             tmp.remove(0);
         }
         wFin =  System.currentTimeMillis();
-        Double wTotal = (double) (wFin - wInicio);
-        System.out.println(String.format("La lista con While tardó %s segundos en terminar", wTotal/1000));
+        double wTotal = (double) (wFin - wInicio);
+        System.out.println("La lista con While tardó " + wTotal/1000 +
+                "segundos en terminar");
 
-        long forInicio,forFin = 0;
+        long forInicio,forFin;
         forInicio = System.currentTimeMillis();
         for (int i = 0; i < aList.size() - 1; i++) {
             System.out.println(aList.get(i));
         }
         forFin =  System.currentTimeMillis();
-        Double forTotal = (double) (forFin - forInicio);
-        System.out.println(String.format("La lista con for tardó %s segundos en terminar", forTotal/1000));
-        long recursivoInicio,recursivoFin = 0;
+        double forTotal = (double) (forFin - forInicio);
+        System.out.println("La lista con for tardó " + forTotal/1000
+                 + " segundos en terminar");
+        long recursivoInicio,recursivoFin;
         recursivoInicio = System.currentTimeMillis();
         porRecursividad(0,aList);
         recursivoFin =  System.currentTimeMillis();
-        Double recursivoTotal = (double) (recursivoFin - recursivoInicio);
-        System.out.println(String.format("La lista recursiva tardó %s segundos en terminar", recursivoTotal/1000));
+        double recursivoTotal = (double) (recursivoFin - recursivoInicio);
+        System.out.println("La lista recursiva tardó " + recursivoTotal/1000
+                 + " segundos en terminar");
 
         ejercicio7D();
-        ArrayList<Integer> capiList = new ArrayList<>(List.of(1,2,3,4,5,6,7,6,5,4,3,2,1));
+        ArrayList<Integer> capiList = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,6,5,4,3,2,1));
         System.out.println("La lista " + capiList + " va a ser evaluada.");
         System.out.println("¿Es capicúa?: " + esCapicua(capiList));
         System.out.println(capiList);
+        
+        //Ejercicio7g)
+
+        System.out.println(EjercicioSucesion.calcularSucesion(6));
+        
+        //Ejercicio7h)
+        ArrayList<Integer> miLista = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        invertirArrayListSinRecursion(miLista);
+        System.out.println(miLista);
+        invertirArrayList(miLista);
+        System.out.println(miLista);
+
+        //Ejercicio7i)
+        LinkedList<Integer> linkedList = new LinkedList<>(Arrays.asList(1,2,3,4,5));
+        LinkedList<Integer> linkedList2 = new LinkedList<>();
+        linkedList2.add(1);
+        linkedList2.addLast(3);
+        linkedList2.addFirst(2);
+        linkedList2.add(6);
+        linkedList2.add(4,4);
+
+        System.out.println("LinkedList: " + linkedList);
+        System.out.println("Suma total: " + sumarLinkedList(linkedList));
+        System.out.println("LinkedList2: " + linkedList2);
+        System.out.println("Suma total: " + sumarLinkedList(linkedList2));
+
+
     }
-    public static boolean esCapicua(ArrayList<Integer> lista) {
-        boolean check = !lista.isEmpty();
-        System.out.println("Primer check: " + check);
-        if ((lista.size() % 2 != 0) && (lista.size() > 1)) {
-            System.out.println("Entró al mod");
-            ArrayList<Integer> tmp = new ArrayList<>(lista);
-            tmp.remove((tmp.size()/2));
-            return (check && esCapicua(tmp));
-        }
-        else  if (check){
-            System.out.println("Entró al check " + lista.size());
-            for (int i = lista.size()/2; i < lista.size(); i++) {
-                System.out.println("Entró al for: " + i + " vez/ces: " + check);
-                System.out.println(lista.get(lista.size()-i-1));
-                System.out.println((lista.get(i)));
-                if(!lista.get(lista.size()-i-1).equals(lista.get(i))) {
-                    check = false;
-                    break;
-                }
-            }
-        }
-        return check;
+
+    private static int  sumarLinkedList (LinkedList<Integer> linkedList) {
+        Iterator<Integer> nodo = linkedList.iterator();
+        return sumarNodos(nodo);
     }
+
+    private static int sumarNodos(Iterator<Integer> nodo) {
+        if (!nodo.hasNext()) {
+            return 0;
+        }
+        else {
+            return nodo.next() + sumarNodos(nodo);
+        }
+    }
+
+    public static void invertirArrayList(ArrayList<Integer> miLista) {
+		invertirArray(miLista,0);
+		
+	}
+	private static void invertirArray(ArrayList<Integer> miLista, int pos) {
+		if (pos < miLista.size()/2) {
+			Integer aux = miLista.get(pos);
+    		miLista.set(pos, miLista.get(miLista.size()-1-pos));
+    		miLista.set(miLista.size()-1-pos, aux);
+    		invertirArray(miLista,pos+1);
+		}
+		
+	}
+	
+	public static boolean esCapicua(ArrayList<Integer> lista) {
+		return esCapicuaRecursivo(lista,0);
+	}
+	private static boolean esCapicuaRecursivo(ArrayList<Integer> lista, int pos) {
+		if(pos < lista.size()/2) {
+			
+			return (esCapicuaRecursivo(lista,pos+1) && (lista.get(pos).equals(lista.get(lista.size()-1-pos))));
+		}
+		else return !lista.isEmpty();
+	}
+
     public static void ejercicio7D() {
         //Creación de la lista de 3 estudiantes
-        List<Estudiante> estudiantes = new ArrayList<Estudiante>(Arrays.asList(
+        List<Estudiante> estudiantes = new ArrayList<>(Arrays.asList(
             new Estudiante("estudiante1", "uno","e1@mail.com","calle 1",1),
             new Estudiante("estudiante2", "dos","e2@mail.com","calle 2",2),
             new Estudiante("estudiante3", "tres","e3@mail.com","calle 3",3)
@@ -131,6 +175,13 @@ public class TestArrayList {
             System.out.println(e.tusDatos());
         }
 
+    }
+    public static void invertirArrayListSinRecursion(ArrayList<Integer> lista) {
+    	for(int pos = 0; pos < lista.size()/2; pos++) {
+    		Integer aux = lista.get(pos);
+    		lista.set(pos, lista.get(lista.size()-1-pos));
+    		lista.set(lista.size()-1-pos, aux);
+    	}
     }
 
     public static void agregarUnEstudiante(List<Estudiante> estudiantes) {
