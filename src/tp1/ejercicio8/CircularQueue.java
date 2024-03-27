92ad033ae5435927bb83307b82d20449da6820e8
JavierCostas81 <javiercostas1981@gmail.com>
2024-03-27 01:36:13 -0300

    TP resuelto

--- src/tp1/ejercicio8/CircularQueue.java
+++
@@ -1,9 +0,0 @@
-package tp1.ejercicio8;
-//Ejercicio 8b
-public class CircularQueue<E> extends Queue{
-    public E shift() throws IllegalAccessException {
-        E aux = (E) this.dequeue();
-        this.enqueue(aux);
-        return aux;
-    }
-}
