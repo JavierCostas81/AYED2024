0a716279f50ad42a9d2615ec0ce148a611016335
JavierCostas81 <javiercostas1981@gmail.com>
2024-04-06 18:42:34 -0300

    TP2 Ejercicio 6

---
+++ src/tp2/Ejercicio6/Transformacion.java
@@ -0,0 +1,36 @@
+package tp2.Ejercicio6;
+
+import tp2.ejercicio1y2.BinaryTree;
+
+public class Transformacion {
+    BinaryTree<Integer> binaryTree;
+
+    public Transformacion(BinaryTree<Integer> ab) {
+        this.binaryTree = ab;
+    }
+
+    public BinaryTree<Integer> suma () {
+        BinaryTree<Integer> returnAB;
+        if (binaryTree == null) {
+            return returnAB = new BinaryTree<Integer>(0);
+        }
+        returnAB = sumarHijos(binaryTree);
+        return returnAB;
+    }
+
+    private BinaryTree<Integer> sumarHijos(BinaryTree<Integer> arbol) {
+        if (arbol.hasLeftChild()) {
+            arbol.setData(arbol.getLeftChild().getData() + sumarHijos(arbol.getLeftChild()).getData());
+        }
+        else {
+            arbol.setData(0);
+        }
+        if (arbol.hasRightChild()) {
+            arbol.setData(arbol.getData() + arbol.getRightChild().getData() + sumarHijos(arbol.getRightChild()).getData()) ;
+        }
+        else {
+            arbol.setData(0);
+        }
+        return arbol;
+    }
+}
--- src/tp2/ejercicio1y2/TestBinaryTree.java
+++ src/tp2/ejercicio1y2/TestBinaryTree.java
@@ -3,6 +3,7 @@ package tp2.ejercicio1y2;
 import tp1.ejercicio8.Queue;
 import tp2.Ejercicio4.RedBinariaLlena;
 import tp2.Ejercicio5.ProfundidadDeArbolBinario;
+import tp2.Ejercicio6.Transformacion;
 import tp2.ejercicio3.ContadorArbol;
 
 import java.util.ArrayList;
@@ -40,13 +41,13 @@ public class TestBinaryTree {
         k2.addRightChild(k3);*/
         System.out.println("La cantidad de hojas de " + a + " es: " + a.contarHojas());
         Queue<Integer> cola = new Queue<>();
-        imprimirArbol(a,cola);
+        imprimirArbol(a, cola);
 
         while (!cola.isEmpty()) {
             System.out.print(cola.dequeue() + " ");
         }
         System.out.println(" ");
-        a.entreNiveles(2,2);
+        a.entreNiveles(2, 2);
         /*imprimirArbol(a.espejo(),cola);
         while (!cola.isEmpty()) {
             System.out.print(cola.dequeue() + " ");
@@ -68,8 +69,29 @@ public class TestBinaryTree {
         ProfundidadDeArbolBinario<Integer> profundidadDeArbolBinario = new ProfundidadDeArbolBinario<>(a);
         int p = 2;
         System.out.println("La suma de los nodos de profundidad " + p + " es " + profundidadDeArbolBinario.sumaElementosProfundidad(p));
-
-
+        BinaryTree<Integer> aa = new BinaryTree<>(1);
+        BinaryTree<Integer> bb = new BinaryTree<>(2);
+        BinaryTree<Integer> cc = new BinaryTree<>(3);
+        BinaryTree<Integer> dd = new BinaryTree<>(4);
+        BinaryTree<Integer> ee = new BinaryTree<>(5);
+        BinaryTree<Integer> ff = new BinaryTree<>(6);
+        BinaryTree<Integer> gg = new BinaryTree<>(7);
+        BinaryTree<Integer> hh = new BinaryTree<>(8);
+        aa.addLeftChild(bb);
+        aa.addRightChild(cc);
+        bb.addRightChild(dd);
+        cc.addLeftChild(ee);
+        cc.addRightChild(ff);
+        ee.addLeftChild(gg);
+        ee.addRightChild(hh);
+        Transformacion arbolTransformado = new Transformacion(aa);
+        BinaryTree<Integer> aaa = arbolTransformado.suma();
+        Queue<String> cola2 = new Queue<>();
+        imprimirArbol2(aaa, cola2);
+        while (!cola2.isEmpty()) {
+            System.out.print(cola2.dequeue() + " ");
+        }
+        System.out.println(" ");
     }
 
     public static void imprimirArbol(BinaryTree<Integer> t, Queue<Integer> cola) {
@@ -80,5 +102,16 @@ public class TestBinaryTree {
         if(t.hasRightChild()){
             imprimirArbol(t.getRightChild(),cola);
         }
+    }
+    public static void imprimirArbol2(BinaryTree<Integer> t, Queue<String> cola) {
+        cola.enqueue(t.getData().toString());
+        if (t.hasLeftChild()) {
+            imprimirArbol2(t.getLeftChild(),cola);
+        }
+        else if (!t.isLeaf()) { cola.enqueue("null"); }
+        if(t.hasRightChild()){
+            imprimirArbol2(t.getRightChild(),cola);
+        }
+        else if (!t.isLeaf()) { cola.enqueue("null"); }
     }
 }
