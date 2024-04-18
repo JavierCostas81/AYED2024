e825fef81c6c68a71a6e1c497af897ec98994a59
JavierCostas81 <javiercostas1981@gmail.com>
2024-04-17 22:04:45 -0300

    TP3 Ejercicios 1 y 2

---
+++ src/tp3/ejercicio01/TestTP3.java
@@ -0,0 +1,52 @@
+package tp3.ejercicio01;
+
+
+import tp3.ejercicio02.RecorridosAG;
+
+import java.util.List;
+
+public class TestTP3 {
+    public static void main (String[] args) throws IllegalAccessException {
+        System.out.println("Ejercicio 2:");
+        RecorridosAG recorridosAG = new RecorridosAG();
+        List<Integer> listaResultadoEj2;
+        GeneralTree<Integer> a = new GeneralTree<>(1);        GeneralTree<Integer> b = new GeneralTree<>(2);
+        GeneralTree<Integer> c = new GeneralTree<>(3);        GeneralTree<Integer> d = new GeneralTree<>(4);
+        GeneralTree<Integer> e = new GeneralTree<>(5);        GeneralTree<Integer> f = new GeneralTree<>(6);
+        GeneralTree<Integer> g = new GeneralTree<>(7);        GeneralTree<Integer> h = new GeneralTree<>(8);
+        GeneralTree<Integer> i = new GeneralTree<>(9);        GeneralTree<Integer> j = new GeneralTree<>(10);
+        GeneralTree<Integer> k = new GeneralTree<>(11);       GeneralTree<Integer> l = new GeneralTree<>(12);
+        a.addChild(b);        a.addChild(c);        a.addChild(d);
+        b.addChild(e);
+        d.addChild(f);        d.addChild(g);
+        e.addChild(h);        e.addChild(i);        e.addChild(j);
+        g.addChild(k);        g.addChild(l);
+        listaResultadoEj2 = recorridosAG.numerosImparesMayoresQuePreOrden(a,1);
+        if (listaResultadoEj2.isEmpty()) {
+            System.out.println("Empty List");
+        } else {
+            System.out.println("PreOrden: " + listaResultadoEj2);
+        }
+        listaResultadoEj2.clear();
+        listaResultadoEj2 = recorridosAG.numerosImparesMayoresQueInOrden(a,1);
+        if (listaResultadoEj2.isEmpty()) {
+            System.out.println("Empty List");
+        } else {
+            System.out.println("InOrden: " + listaResultadoEj2);
+        }
+        listaResultadoEj2.clear();
+        listaResultadoEj2 = recorridosAG.numerosImparesMayoresQuePostOrden(a,1);
+        if (listaResultadoEj2.isEmpty()) {
+            System.out.println("Empty List");
+        } else
+        System.out.println("PostOrden: " + listaResultadoEj2);
+        listaResultadoEj2.clear();
+        listaResultadoEj2 = recorridosAG.numerosImparesMayoresQuePorNiveles(a,1);
+        if (listaResultadoEj2.isEmpty()) {
+            System.out.println("Empty List");
+        } else
+            System.out.println("Por Niveles: " + listaResultadoEj2);
+
+
+    }
+}
---
+++ src/tp3/ejercicio02/RecorridosAG.java
@@ -0,0 +1,95 @@
+package tp3.ejercicio02;
+
+import tp1.ejercicio08.Queue;
+import tp3.ejercicio01.GeneralTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecorridosAG {
+
+    public List<Integer> numerosImparesMayoresQuePreOrden (GeneralTree<Integer> a, Integer n) {
+//Método que retorna una lista con los elementos impares del árbol “a” que sean mayores al valor “n”
+//pasados como parámetros, recorrido en preorden.
+        List<Integer> resultado = new ArrayList<>();
+        if ((a != null) && !a.isEmpty()) {
+            int b = a.getData();
+            if (b % 2 != 0 && b > n) {
+                resultado.add(b);
+            }
+            for (GeneralTree<Integer> childs: a.getChildren()) {
+                resultado.addAll(numerosImparesMayoresQuePreOrden(childs,n));
+            }
+
+        }
+        return resultado;
+    }
+
+    public List<Integer> numerosImparesMayoresQueInOrden (GeneralTree <Integer> a,Integer n) {
+  /*  Método que retorna una lista con los elementos impares del árbol “a” que sean mayores al valor “n”
+    pasados como parámetros, recorrido en inorden.*/
+        List<Integer> resultado = new ArrayList<>();
+        if ((a != null) && !a.isEmpty()) {
+            if (a.hasChildren()) {
+                for (int i = 0; i < a.getChildren().size(); i++) {
+                    if (i == 0) {
+                        resultado.addAll(numerosImparesMayoresQueInOrden(a.getChildren().get(i), n));
+                        int b = a.getData();
+                        if (b % 2 != 0 && b > n) {
+                            resultado.add(b);
+                        }
+                    } else {
+                        resultado.addAll(numerosImparesMayoresQueInOrden(a.getChildren().get(i), n));
+                    }
+                }
+            }
+            else
+            {
+                int b = a.getData();
+                if (b % 2 != 0 && b > n) {
+                    resultado.add(b);
+                }
+            }
+        }
+        return resultado;
+    }
+    public List<Integer> numerosImparesMayoresQuePostOrden (GeneralTree <Integer> a,Integer n) {
+    /*Método que retorna una lista con los elementos impares del árbol “a” que sean mayores al valor “n”
+    pasados como parámetros, recorrido en postorden.*/
+        List<Integer> resultado = new ArrayList<>();
+        if ((a != null) && !a.isEmpty()) {
+            for (GeneralTree<Integer> childs: a.getChildren()) {
+                resultado.addAll(numerosImparesMayoresQuePostOrden(childs, n));
+            }
+            int b = a.getData();
+            if (b % 2 != 0 && b > n) {
+                resultado.add(b);
+            }
+        }
+        return resultado;
+    }
+    public List<Integer> numerosImparesMayoresQuePorNiveles(GeneralTree <Integer> a,Integer n) throws IllegalAccessException {
+   /* Método que retorna una lista con los elementos impares del árbol “a” que sean mayores al valor “n”
+    pasados como parámetros, recorrido por niveles.*/
+        List<Integer> resultado = new ArrayList<>();
+        Queue<GeneralTree<Integer>> cola = new Queue<>();
+        GeneralTree<Integer> aG;
+        cola.enqueue(a);
+        cola.enqueue(null);
+        while (!cola.isEmpty()) {
+            if (((aG = cola.dequeue()) != null) && (!aG.isEmpty())) {
+                int b = aG.getData();
+                if (b % 2 != 0 && b > n) {
+                    resultado.add(b);
+                }
+                for (GeneralTree<Integer> gT : aG.getChildren()
+                ) {
+                    cola.enqueue(gT);
+                }
+            } else if (!cola.isEmpty()) {
+                cola.enqueue(null);
+            }
+        }
+        return resultado;
+    }
+}
