00d3fb528b2c51cdaff50989a7d0cf9cabcb5069
JavierCostas81 <javiercostas1981@gmail.com>
2024-04-30 00:27:12 -0300

    TP3 Finalizado

--- src/tp3/ejercicio09/ParcialArboles.java
+++ src/tp3/ejercicio09/ParcialArboles.java
@@ -1,7 +1,9 @@
 package tp3.ejercicio09;
 
+import tp1.ejercicio08.Queue;
 import tp3.ejercicio01.GeneralTree;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*Implemente en la clase ParcialArboles el método:
@@ -12,23 +14,79 @@ public static boolean esDeSeleccion (GeneralTree<Integer> arbol)
         Para este otro árbol se debería retornar false (el árbol con raíz 18 tiene un hijo con valor mínimo 14 )
 */
 public class ParcialArboles {
-    public static boolean esDeSeleccion (GeneralTree<Integer> arbol) {
+    public static boolean esDeSeleccion(GeneralTree<Integer> arbol) {
         boolean ok = false;
         if (arbol != null && !arbol.isEmpty()) {
-                ok = minimoHijos(arbol);
-                for (GeneralTree<Integer> child : arbol.getChildren()) {
-                    if (!child.isLeaf())
+            ok = minimoHijos(arbol);
+            for (GeneralTree<Integer> child : arbol.getChildren()) {
+                if (!child.isLeaf())
                     ok = ok && esDeSeleccion(child);
-                }
             }
+        }
         return ok;
     }
 
     private static boolean minimoHijos(GeneralTree<Integer> padre) {
         int minimo = Integer.MAX_VALUE;
         for (GeneralTree<Integer> child : padre.getChildren()) {
-            minimo = Math.min(child.getData(),minimo);
+            minimo = Math.min(child.getData(), minimo);
         }
         return minimo == padre.getData();
+    }
+
+    public static List<Integer> resolver(GeneralTree<Integer> arbol) {
+        List<Integer> lista = new ArrayList<>();
+        List<Integer> tLista = new ArrayList<>();
+        resolver(arbol,0,0, 0, lista, tLista);
+        return lista;
+    }
+
+    private static int resolver (GeneralTree<Integer> arbol,int nivel, int suma, int max, List<Integer> lista, List<Integer> tLista) {
+        if (arbol != null && !arbol.isEmpty()) {
+            if (arbol.getData() == 1) {
+                tLista.add(arbol.getData());
+            }
+            suma += (arbol.getData() * nivel);
+            for (GeneralTree<Integer> ag : arbol.getChildren()) {
+                max = resolver(ag, nivel + 1, suma , max, lista,tLista);
+            }
+            if (arbol.isLeaf()) {
+                if (max<suma) {
+                    max = suma;
+                    lista.clear();
+                    lista.addAll(tLista);
+                }
+            }
+            if (arbol.getData() == 1) {
+                tLista.remove(tLista.size()-1);
+            }
+        }
+        return Math.max(max,suma);
+    }
+
+    public static boolean resolver2(GeneralTree<Integer> arbol) throws IllegalAccessException {
+        Queue<GeneralTree<Integer>> cola = new Queue<>();
+        GeneralTree<Integer> agTemp;
+        boolean test = (arbol != null && !arbol.isEmpty());
+        int nivel = test ? 1 : -1;
+        cola.enqueue(arbol);
+        cola.enqueue(null);
+        while (!cola.isEmpty() && test) {
+            int nodosNivel = 0;
+            while ((agTemp = cola.dequeue()) != null) {
+                for (GeneralTree<Integer> child : agTemp.getChildren()) {
+                     nodosNivel++;
+                     if (!child.isLeaf()){
+                         cola.enqueue(child);
+                     }
+                }
+                test = (nivel + 1) == nodosNivel;
+            }
+            if (!cola.isEmpty() && test) {
+                cola.enqueue(null);
+                nivel++;
+            }
+        }
+        return test;
     }
 }
--- src/tp3/ejercicio09/TestParcialArboles.java
+++ src/tp3/ejercicio09/TestParcialArboles.java
@@ -5,7 +5,7 @@ import tp3.ejercicio01.GeneralTree;
 import java.util.Arrays;
 
 public class TestParcialArboles {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         ejercicio9();
         ejercicio10();
         ejercicio11();
@@ -35,8 +35,53 @@ public class TestParcialArboles {
     }
 
     private static void ejercicio10() {
+        GeneralTree<Integer> a = new GeneralTree<>(1);
+        GeneralTree<Integer> b = new GeneralTree<>(0);
+        GeneralTree<Integer> c = new GeneralTree<>(1);
+        GeneralTree<Integer> d = new GeneralTree<>(1);
+        GeneralTree<Integer> e = new GeneralTree<>(1);
+        GeneralTree<Integer> f = new GeneralTree<>(1);
+        GeneralTree<Integer> g = new GeneralTree<>(1);
+        GeneralTree<Integer> h = new GeneralTree<>(0);
+        GeneralTree<Integer> i = new GeneralTree<>(0);
+        GeneralTree<Integer> j = new GeneralTree<>(1);
+        GeneralTree<Integer> k = new GeneralTree<>(1);
+        GeneralTree<Integer> l = new GeneralTree<>(1);
+        GeneralTree<Integer> m = new GeneralTree<>(0);
+        GeneralTree<Integer> n = new GeneralTree<>(0);
+        GeneralTree<Integer> o = new GeneralTree<>(1);
+        GeneralTree<Integer> p = new GeneralTree<>(0);
+        GeneralTree<Integer> q = new GeneralTree<>(0);
+        n.setChildren(Arrays.asList(p,q));
+        m.addChild(o);
+        i.addChild(n);
+        h.addChild(m);
+        e.setChildren(Arrays.asList(j,k,l));
+        d.addChild(i);
+        c.setChildren(Arrays.asList(g,h));
+        b.setChildren(Arrays.asList(e,f));
+        a.setChildren(Arrays.asList(b,c,d));
+        System.out.println("Ejercicio 10:\nLa lista filtrada es: " + ParcialArboles.resolver(a));
     }
 
-    private static void ejercicio11() {
+    private static void ejercicio11() throws IllegalAccessException {
+        GeneralTree<Integer> a = new GeneralTree<>(2);  GeneralTree<Integer> b = new GeneralTree<>(1);
+        GeneralTree<Integer> c = new GeneralTree<>(25); GeneralTree<Integer> d = new GeneralTree<>(5);
+        GeneralTree<Integer> e = new GeneralTree<>(4);  GeneralTree<Integer> f = new GeneralTree<>(13);
+        GeneralTree<Integer> g = new GeneralTree<>(18); GeneralTree<Integer> h = new GeneralTree<>(7);
+        GeneralTree<Integer> i = new GeneralTree<>(11); GeneralTree<Integer> j = new GeneralTree<>(3);
+        GeneralTree<Integer> k = new GeneralTree<>(83); GeneralTree<Integer> l = new GeneralTree<>(33);
+        GeneralTree<Integer> m = new GeneralTree<>(12); GeneralTree<Integer> n = new GeneralTree<>(17);
+        GeneralTree<Integer> o = new GeneralTree<>(9);
+        j.setChildren(Arrays.asList(l,m,n,o));
+        g.addChild(k);
+        e.setChildren(Arrays.asList(h,i,j));
+        d.addChild(g);
+        c.addChild(f);
+        b.setChildren(Arrays.asList(d,e));
+        a.setChildren(Arrays.asList(b,c));
+        System.out.println("Ejercicio 11:\na) ¿Es creiciente?: " + ParcialArboles.resolver2(a));
+        e.setChildren(Arrays.asList(h,j));
+        System.out.println("b) ¿Es creciente?: " + ParcialArboles.resolver2(a));
     }
 }
